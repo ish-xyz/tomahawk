a32579f035e9b1de66087cb81289076f091c0aa1
Isham Araia <ishamaraia@gmail.com>
2019-12-07 13:48:42 +0000

    General minor fixes, adjustments

--- src/controllers.tf
+++ src/controllers.tf
@@ -1,205 +1,205 @@
 resource "tls_private_key" "bootstrap_key" {
-	algorithm = "RSA"
+  algorithm = "RSA"
 }
 
 resource "local_file" "bootstrap_private_key" {
-	content = tls_private_key.bootstrap_key.private_key_pem
-	filename = "${path.module}/installer/.bootstrap.pem"
-	file_permission = "0600"
+  content         = tls_private_key.bootstrap_key.private_key_pem
+  filename        = "${path.module}/installer/.bootstrap.pem"
+  file_permission = "0600"
 }
 
 resource "aws_key_pair" "k8s_bootstrap" {
-	key_name   = "k8s_bootstrap"
-	public_key = tls_private_key.bootstrap_key.public_key_openssh
+  key_name   = "k8s_bootstrap"
+  public_key = tls_private_key.bootstrap_key.public_key_openssh
 }
 
 resource "aws_instance" "controllers" {
 
-	ami = var.controllers_ami
-	count = var.controllers_count
-	key_name = aws_key_pair.k8s_bootstrap.key_name
-	instance_type = "t2.micro"
+  ami           = var.controllers_ami
+  count         = var.controllers_count
+  key_name      = aws_key_pair.k8s_bootstrap.key_name
+  instance_type = "t2.micro"
 
-  	security_groups = [
-		  "${aws_security_group.controllers.name}"
-	]
+  security_groups = [
+    "${aws_security_group.controllers.name}"
+  ]
 
-	tags = {
-		Name = "kube-controller-${count.index}"
-		role = "master"
-		environment = var.environment
-	}
+  tags = {
+    Name        = "kube-controller-${count.index}"
+    role        = "master"
+    environment = var.environment
+  }
 
-	connection {
-		type     	= "ssh"
-		user     	= "centos"
-		private_key = tls_private_key.bootstrap_key.private_key_pem
-		host     	= self.public_ip
-	}
+  connection {
+    type        = "ssh"
+    user        = "centos"
+    private_key = tls_private_key.bootstrap_key.private_key_pem
+    host        = self.public_ip
+  }
 
-	provisioner "remote-exec" {
-		inline = [
-			"sudo hostnamectl set-hostname controller-${count.index}",
-			"mkdir -p ~/bootstrap/certs"
-		]
-	}
+  provisioner "remote-exec" {
+    inline = [
+      "sudo hostnamectl set-hostname controller-${count.index}",
+      "mkdir -p ~/bootstrap/certs"
+    ]
+  }
 }
 
 resource "null_resource" "import_bootstrap_files" {
-	count = var.controllers_count
+  count = var.controllers_count
 
-	triggers = {
-		cluster_instance_ids = "${join(",", aws_instance.controllers.*.id)}"
-	}
+  triggers = {
+    cluster_instance_ids = "${join(",", aws_instance.controllers.*.id)}"
+  }
 
-	connection {
-		type     	= "ssh"
-		user     	= "centos"
-		private_key = tls_private_key.bootstrap_key.private_key_pem
-		host     	= element(aws_instance.controllers.*.public_ip, count.index)
-	}
+  connection {
+    type        = "ssh"
+    user        = "centos"
+    private_key = tls_private_key.bootstrap_key.private_key_pem
+    host        = element(aws_instance.controllers.*.public_ip, count.index)
+  }
 
-    #Import bootstrap scripts
-	provisioner "file" {
-		source      = "${path.module}/installer/etcd_bootstrap.sh"
-		destination = "~/bootstrap/etcd_bootstrap.sh"
-	}
+  #Import bootstrap scripts
+  provisioner "file" {
+    source      = "${path.module}/installer/etcd_bootstrap.sh"
+    destination = "~/bootstrap/etcd_bootstrap.sh"
+  }
 
-    #Import certs and keys
-	provisioner "file" {
-		content     = module.init-ca.ca_cert
-		destination = "~/bootstrap/certs/ca.pem"
-    }
+  #Import certs and keys
+  provisioner "file" {
+    content     = module.init-ca.ca_cert
+    destination = "~/bootstrap/certs/ca.pem"
+  }
 
-	provisioner "file" {
-		content     = module.init-ca.ca_key
-		destination = "~/bootstrap/certs/ca-key.pem"
-	}
+  provisioner "file" {
+    content     = module.init-ca.ca_key
+    destination = "~/bootstrap/certs/ca-key.pem"
+  }
 
-	provisioner "file" {
-		content     = module.admin.cert
-		destination = "~/bootstrap/certs/admin.pem"
-	}
+  provisioner "file" {
+    content     = module.admin.cert
+    destination = "~/bootstrap/certs/admin.pem"
+  }
 
-	provisioner "file" {
-		content     = module.admin.key
-		destination = "~/bootstrap/certs/admin-key.pem"
-	}
+  provisioner "file" {
+    content     = module.admin.key
+    destination = "~/bootstrap/certs/admin-key.pem"
+  }
 
-	provisioner "file" {
-		content     = module.kube-controller-manager.cert
-		destination = "~/bootstrap/certs/kube-controller-manager.pem"
-	}
+  provisioner "file" {
+    content     = module.kube-controller-manager.cert
+    destination = "~/bootstrap/certs/kube-controller-manager.pem"
+  }
 
-	provisioner "file" {
-		content     = module.kube-controller-manager.key
-		destination = "~/bootstrap/certs/kube-controller-manager-key.pem"
-	}
+  provisioner "file" {
+    content     = module.kube-controller-manager.key
+    destination = "~/bootstrap/certs/kube-controller-manager-key.pem"
+  }
 
-	provisioner "file" {
-		content     = module.kube-proxy.cert
-		destination = "~/bootstrap/certs/kube-proxy.pem"
-	}
+  provisioner "file" {
+    content     = module.kube-proxy.cert
+    destination = "~/bootstrap/certs/kube-proxy.pem"
+  }
 
-	provisioner "file" {
-		content     = module.kube-proxy.key
-		destination = "~/bootstrap/certs/kube-proxy-key.pem"
-	}
+  provisioner "file" {
+    content     = module.kube-proxy.key
+    destination = "~/bootstrap/certs/kube-proxy-key.pem"
+  }
 
-	provisioner "file" {
-		content     = module.kube-scheduler.cert
-		destination = "~/bootstrap/certs/kube-scheduler.pem"
-	}
+  provisioner "file" {
+    content     = module.kube-scheduler.cert
+    destination = "~/bootstrap/certs/kube-scheduler.pem"
+  }
 
-	provisioner "file" {
-		content     = module.kube-scheduler.key
-		destination = "~/bootstrap/certs/kube-scheduler-key.pem"
-	}
+  provisioner "file" {
+    content     = module.kube-scheduler.key
+    destination = "~/bootstrap/certs/kube-scheduler-key.pem"
+  }
 
-	provisioner "file" {
-		content     = module.service-accounts.cert
-		destination = "~/bootstrap/certs/service-accounts.pem"
-	}
+  provisioner "file" {
+    content     = module.service-accounts.cert
+    destination = "~/bootstrap/certs/service-accounts.pem"
+  }
 
-	provisioner "file" {
-		content     = module.service-accounts.key
-		destination = "~/bootstrap/certs/service-accounts-key.pem"
-	}
+  provisioner "file" {
+    content     = module.service-accounts.key
+    destination = "~/bootstrap/certs/service-accounts-key.pem"
+  }
 
-	provisioner "file" {
-	 	content     = module.kubernetes.cert
-	 	destination = "~/bootstrap/certs/kubernetes.pem"
-	}
+  provisioner "file" {
+    content     = module.kubernetes.cert
+    destination = "~/bootstrap/certs/kubernetes.pem"
+  }
 
-	provisioner "file" {
-	 	content     = module.kubernetes.key
-	 	destination = "~/bootstrap/certs/kubernetes-key.pem"
-	}
+  provisioner "file" {
+    content     = module.kubernetes.key
+    destination = "~/bootstrap/certs/kubernetes-key.pem"
+  }
 
 
 }
 
 resource "null_resource" "bootstrap-etcd" {
-	count = var.controllers_count
-	depends_on = [null_resource.import_bootstrap_files]
+  count      = var.controllers_count
+  depends_on = [null_resource.import_bootstrap_files]
 
-	triggers = {
-		cluster_instance_ids = "${join(",", aws_instance.controllers.*.id)}"
-	}
+  triggers = {
+    cluster_instance_ids = "${join(",", aws_instance.controllers.*.id)}"
+  }
 
-	connection {
-		type     	= "ssh"
-		user     	= "centos"
-		private_key = tls_private_key.bootstrap_key.private_key_pem
-		host     	= element(aws_instance.controllers.*.public_ip, count.index)
-	}
+  connection {
+    type        = "ssh"
+    user        = "centos"
+    private_key = tls_private_key.bootstrap_key.private_key_pem
+    host        = element(aws_instance.controllers.*.public_ip, count.index)
+  }
 
-	provisioner "remote-exec" {
-		inline = [
-			"chmod +x ~/bootstrap/etcd_bootstrap.sh",
-			"cd ~/bootstrap && sudo ./etcd_bootstrap.sh \"${join(" ", aws_instance.controllers.*.private_ip)}\""
-		]
-	}
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x ~/bootstrap/etcd_bootstrap.sh",
+      "cd ~/bootstrap && sudo ./etcd_bootstrap.sh \"${join(" ", aws_instance.controllers.*.private_ip)}\""
+    ]
+  }
 }
 
 resource "aws_security_group" "controllers" {
-	name = "kube-controllers"
-	vpc_id = var.vpc_id
+  name   = "kube-controllers"
+  vpc_id = var.vpc_id
 }
 
 resource "aws_security_group_rule" "allow_egress_all" {
-  type              = "egress"
-  to_port           = 0
-  from_port         = 0
-  protocol          = "-1"
-  cidr_blocks	= ["0.0.0.0/0"]
+  type        = "egress"
+  to_port     = 0
+  from_port   = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.controllers.id
 }
 
 resource "aws_security_group_rule" "allow_etcd_ext" {
-  type			= "ingress"
-  from_port		= 2380
-  to_port		= 2380
-  protocol		= "tcp"
-  cidr_blocks	= ["0.0.0.0/0"]
+  type        = "ingress"
+  from_port   = 2380
+  to_port     = 2380
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.controllers.id
 }
 
 resource "aws_security_group_rule" "allow_ssh_ext" {
-  type			= "ingress"
-  from_port		= 22
-  to_port		= 22
-  protocol		= "tcp"
-  cidr_blocks	= ["0.0.0.0/0"]
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.controllers.id
 }
 
 resource "random_string" "ekey" {
-  length = 32
-  special = true
+  length           = 32
+  special          = true
   override_special = "/@£$"
 }
 
@@ -212,22 +212,22 @@ data "template_file" "ekey" {
 
 
 resource "null_resource" "scp_kube_configs" {
-	count = var.controllers_count
+  count = var.controllers_count
 
-	triggers = {
-		cluster_instance_ids = "${join(",", aws_instance.controllers.*.id)}",
-		template_file_id	 = data.template_file.ekey.rendered
-	}
+  triggers = {
+    cluster_instance_ids = "${join(",", aws_instance.controllers.*.id)}",
+    template_file_id     = data.template_file.ekey.rendered
+  }
 
-	connection {
-		type     	= "ssh"
-		user     	= "centos"
-		private_key = tls_private_key.bootstrap_key.private_key_pem
-		host     	= element(aws_instance.controllers.*.public_ip, count.index)
-	}
+  connection {
+    type        = "ssh"
+    user        = "centos"
+    private_key = tls_private_key.bootstrap_key.private_key_pem
+    host        = element(aws_instance.controllers.*.public_ip, count.index)
+  }
 
-	provisioner "file" {
-		content     = data.template_file.ekey.rendered
-		destination = "~/bootstrap/encryption-config.yaml"
-	}
+  provisioner "file" {
+    content     = data.template_file.ekey.rendered
+    destination = "~/bootstrap/encryption-config.yaml"
+  }
 }
--- src/variables.tf
+++ src/variables.tf
@@ -1,51 +1,39 @@
 ##Global
 variable "vpc_id" {
-	type = string
-	default = "vpc-f670c791"
+  type    = string
+  default = "vpc-f670c791"
 }
 
 variable "environment" {
-	type = string
-	default = "development"
+  type    = string
+  default = "development"
 }
 
 ##Controllers
 variable "controllers_count" {
-	type = number
-	default = 3
+  type    = number
+  default = 3
 }
 
 
 variable "controllers_ami" {
-	type 	= string
-	default = "ami-0334a7a72f69e4d0f"
+  type    = string
+  default = "ami-0334a7a72f69e4d0f"
 }
 
 ##ELB
 
 variable "elb_bucket" {
-	type 	= string
-	default = "kube-controllers-elb"
+  type    = string
+  default = "kube-controllers-elb"
 }
 
 variable "elb_bucket_prefix" {
-	type 	= string
-	default = "logs"
+  type    = string
+  default = "logs"
 }
 
 variable "elb_name" {
-	type 	= string
-	default = "kube-controllers-elb"
+  type    = string
+  default = "kube-controllers-elb"
 }
-# var.alb_subnets
-# var.alb_security_groups
-# var.idle_timeout
-# var.s3_bucket    
-# var.alb_listener_port  
-# var.alb_listener_protocol
-# var.target_group_name  
-# var.svc_port    
-# var.target_group_name    
-# var.target_group_sticky  
-# var.target_group_path    
-# var.target_group_port  
