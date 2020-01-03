0a96130814d19e8ad072a457f0993a6386e3cbe5
Isham Araia <ishamaraia@gmail.com>
2020-01-03 00:04:13 +0000

    Improved local resources creation

--- src/local_resources.tf
+++ src/local_resources.tf
@@ -1,29 +1,47 @@
 resource "local_file" "controllers_ssh" {
   content         = tls_private_key.controllers_ssh.private_key_pem
-  filename        = "${path.module}/local/controllers.pem"
+  filename        = "${path.module}/.local/ssh-controllers.pem"
   file_permission = "0600"
 }
 
 resource "local_file" "workers_ssh" {
   content         = tls_private_key.workers_ssh.private_key_pem
-  filename        = "${path.module}/local/workers.pem"
+  filename        = "${path.module}/.local/ssh-workers.pem"
   file_permission = "0600"
 }
 
 resource "local_file" "ca_certificate" {
   content         = module.init-ca.ca_cert
-  filename        = "${path.module}/local/ca.pem"
+  filename        = "${path.module}/.local/ca.pem"
   file_permission = "0644"
 }
 
-resource "local_file" "ca_key" {
-  content         = module.init-ca.ca_key
-  filename        = "${path.module}/local/ca-key.pem"
+resource "local_file" "admin_key" {
+  content         = module.admin.key
+  filename        = "${path.module}/.local/admin-key.pem"
   file_permission = "0600"
 }
 
-resource "local_file" "admin_kubeconfig" {
-  content         = data.template_file.admin.rendered
-  filename        = "${path.module}/local/admin.kubeconfig"
+resource "local_file" "admin_cert" {
+  content         = module.admin.cert
+  filename        = "${path.module}/.local/admin.pem"
+  file_permission = "0644"
+}
+
+data "template_file" "remote_admin" {
+  template = file("${path.module}/templates/kubeconfig.yml.tpl")
+  vars = {
+    project_name = var.cluster_name
+    client_cert  = base64encode(module.admin.cert)
+    client_key   = base64encode(module.admin.key)
+    ca_cert      = base64encode(module.init-ca.ca_cert)
+    user         = "admin"
+    kube_address = "https://${aws_lb.controllers.dns_name}:6443"
+  }
+}
+
+resource "local_file" "remote_admin" {
+  content         = data.template_file.remote_admin.rendered
+  filename        = "${path.module}/.local/admin.kubeconfig"
   file_permission = "0644"
 }
